5556297016cb3fba08cac234fcb6c2702f449456
Ayca96 <aycakubra1@gmail.com>
2024-08-11 19:37:40 +0200

    "Added axios dependency and implemented API call to fetch data in InterviewAccord component"

--- src/components/InterviewAccord.jsx
+++ src/components/InterviewAccord.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import Accordion from 'react-bootstrap/Accordion';
-import data from "../helper/data.js";
+
 
 const InterviewAccord = () => {
+
+  const [datei, setDatei] = useState([]);
+
+  const BASE_URL = "https://66b7c70c7f7b1c6d8f1cbb62.mockapi.io/interview";
+
+  const getData = async ()=>{
+    const {data} = await axios (BASE_URL);
+    setDatei(data)
+  }
+
+  useEffect (()=>{
+    getData()
+  },[])
+
   return (
     <div className='accord'>
-     
     <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>{data.header}</Accordion.Header>
-        <Accordion.Body>
-         {data.body}
-        </Accordion.Body>
-      </Accordion.Item>
-     
+      {datei.map((bilgi, index) => (
+        <Accordion.Item key={bilgi.id} eventKey={index.toString()}>
+          <Accordion.Header>{bilgi.questions}</Accordion.Header>
+          <Accordion.Body>
+            {bilgi.answers}
+          </Accordion.Body>
+        </Accordion.Item>
+      ))}
     </Accordion>
-  
-    </div>
+  </div>
   )
 }
 
